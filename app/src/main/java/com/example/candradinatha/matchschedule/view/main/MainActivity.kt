cf0508355a3e6cb278eb37a8f216cc00d51477e1
Bagus Candradinatha <candradinatha>
2018-10-20 13:40:05 +0800

    added bottom nav

--- app/src/main/java/com/example/candradinatha/matchschedule/view/main/MainActivity.kt
+++ app/src/main/java/com/example/candradinatha/matchschedule/view/main/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.candradinatha.matchschedule.view.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.candradinatha.matchschedule.FavoriteFragment
+import com.example.candradinatha.matchschedule.MatchFragment
 import com.example.candradinatha.matchschedule.R
+import com.example.candradinatha.matchschedule.R.id.favorites
+import com.example.candradinatha.matchschedule.R.id.matches
 import com.example.candradinatha.matchschedule.adapter.MyPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,9 +16,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentAdapter = MyPagerAdapter(supportFragmentManager)
-        viewpager_main.adapter = fragmentAdapter
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                matches -> {
+                    loadMatchFragment(savedInstanceState)
+                }
+                favorites -> {
+                    loadFavoriteFragment(savedInstanceState)
+                }
+            }
+            true
+        }
+        bottom_navigation.selectedItemId = matches
+    }
 
-        tabs_main.setupWithViewPager(viewpager_main)
+    private fun loadMatchFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, MatchFragment(), MatchFragment::class.java.simpleName)
+                    .commit()
+        }
+    }
+
+    private fun loadFavoriteFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null){
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, FavoriteFragment(), FavoriteFragment::class.java.simpleName)
+                    .commit()
+        }
     }
 }
